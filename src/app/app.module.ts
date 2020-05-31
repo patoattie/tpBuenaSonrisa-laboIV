ddea7578b1d5d4a0cfe5d938b050a93c908bb82c
patoattie <patoattie@gmail.com>
2020-05-31 20:30:02 -0300

    quito ngx-auth-firebaseui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,6 @@ import { environment } from '../environments/environment';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
-import { NgxAuthFirebaseUIModule } from 'ngx-auth-firebaseui';
-import { MatPasswordStrengthModule } from '@angular-material-extensions/password-strength';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './componentes/login/login.component';
@@ -32,19 +30,6 @@ export function tokenGetter() {
     AngularFireStorageModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NgxAuthFirebaseUIModule.forRoot(
-      environment.firebaseConfig,
-      () => 'Buena Sonrisa',
-      {
-        enableFirestoreSync: false, // enable/disable autosync users with firestore
-        toastMessageOnAuthSuccess: false,
-        toastMessageOnAuthError: false,
-        passwordMinLength: 6,
-        passwordMaxLength: 60,
-        enableEmailVerification: false
-      }
-    ),
-    MatPasswordStrengthModule,
     JwtModule.forRoot({
       config: {
         tokenGetter
