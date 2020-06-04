d65efc8f1140e77fb0034c8a7ae44f61fa096645
patoattie <patoattie@gmail.com>
2020-06-04 20:29:04 -0300

    toolbar

--- src/app/componentes/toolbar/toolbar.component.ts
+++ src/app/componentes/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-toolbar',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private location: Location
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  public irAtras(): void {
+    if (this.puedeIrAtras()) {
+      this.location.back();
+    }
   }
 
+  public puedeIrAtras(): boolean {
+    return this.location.path() !== '/inicio';
+  }
 }
